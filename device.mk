a94bb1d6893319e632f8e0231cb219485f36a1a4
bulla03x <jyotishmanbaruah67@gmail.com>
2025-09-02 02:27:07 +0530

    Revert "raphael: Bring back Live display"
    This reverts commit df221083723872244636f4ea8921379d4cdfa9c4.

--- device.mk
+++ device.mk
@@ -379,6 +379,10 @@ $(call soong_config_set,lineage_health,charging_control_charging_enabled,0)
 $(call soong_config_set,lineage_health,charging_control_charging_path,/sys/class/power_supply/battery/input_suspend)
 $(call soong_config_set,lineage_health,charging_control_supports_bypass,false)
 
+# Livedisplay
+PRODUCT_PACKAGES += \
+    vendor.lineage.livedisplay@2.1-service.raphael
+
 # Logging
 SPAMMY_LOG_TAGS := \
     MiStcImpl \
@@ -408,10 +412,6 @@ $(call soong_config_set,lineage_health,charging_control_charging_disabled,1)
 $(call soong_config_set,lineage_health,charging_control_charging_enabled,0)
 $(call soong_config_set,lineage_health,charging_control_charging_path,/sys/class/power_supply/battery/input_suspend)
 $(call soong_config_set,lineage_health,charging_control_supports_bypass,false)
-
-# Livedisplay
-PRODUCT_PACKAGES += \
-    vendor.lineage.livedisplay@2.1-service.raphael
 
 # Media
 PRODUCT_PACKAGES += \
